004210a2ff6be4a6627ecbb11d0099860d024715
Adam <adam@adam.com>
2025-03-28 19:01:44 -0500

    play button

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ TITLE_FONT = pygame.font.SysFont("Times New Roman", SCREEN_HEIGHT // 6, bold=Tru
 PLAY_FONT = pygame.font.SysFont("Arial", SCREEN_HEIGHT // 10, bold=True)
 title_surface = TITLE_FONT.render("HISTORY OF EARTH", True, "white")
 play_surface = PLAY_FONT.render("PLAY", True, "green")
+play_surface_pos = (
+    SCREEN_WIDTH * 0.75 - play_surface.get_width() / 2,
+    360
+)
 
 earth = pygame.image.load("Graphics/Earth.png")
 background = pygame.image.load("Graphics/Background.png")
@@ -21,6 +25,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if (
+                event.pos[0] > play_surface_pos[0] and
+                event.pos[1] > play_surface_pos[1] and
+                event.pos[0] < play_surface_pos[0] + play_surface.get_width() and
+                event.pos[1] < play_surface_pos[1] + play_surface.get_height()
+            ):
+                print("Have fun!")
+
 
     screen.fill("black")
 
@@ -30,10 +43,7 @@ while running:
         SCREEN_WIDTH / 2 - title_surface.get_width() / 2,
         40
     ))
-    screen.blit(play_surface, (
-        SCREEN_WIDTH * 0.75 - play_surface.get_width() / 2,
-        360
-    ))
+    screen.blit(play_surface, play_surface_pos)
 
     pygame.display.flip()
 
